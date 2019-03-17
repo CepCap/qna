5a378f66631c707a0ac1d3a5bd4a285db5677694
CepCap <nikinoob@gmail.com>
2019-03-17 02:40:46 -0700

    Removing before_action for questions using DecentExposure gem

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,32 @@
 class QuestionsController < ApplicationController
+  expose :questions, -> { Question.all }
+  expose :question
 
-  def index
-    @questions = Question.all
+  def create
+    @question = Question.create(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
   end
 
+  def update
+    if question.update(question_params)
+      redirect_to question
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    question.destroy
+    redirect_to questions_path
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,17 +1,75 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
-  describe 'GET #index' do
-    let(:questions) { create_list(:question, 3) }
+  let(:question) { create(:question) }
 
-    before { get :index }
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new question in DB' do
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
 
-    it 'populates an array of all questions' do
-      expect(assigns(:questions)).to match_array(questions)
+      it 'redirects to new question' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:question)
+      end
     end
 
-    it 'render index view' do
-      expect(response).to render_template :index
+    context 'with invalid attributes' do
+      it 'doesnt saves a new question in DB' do
+        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
+      end
+
+      it 're-renders #new view' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'with valid attributes' do
+
+      it "changes question attributes" do
+        patch :update, params: { id: question, question: { title: "New title", body: "New body" } }
+        question.reload
+
+        expect(question.title).to eq "New title"
+        expect(question.body).to eq "New body"
+      end
+
+      it 'redirects to updated question' do
+        patch :update, params: { id: question, question: { title: "New title", body: "New body" } }
+        expect(response).to redirect_to question
+      end
+    end
+
+    context 'with invalid attributes'do
+      before { patch :update, params: { id: question, question: attributes_for(:question, :invalid) } }
+
+      it 'doesnt changes question attributes' do
+        question.reload
+
+        expect(question.title).to eq "MyString"
+        expect(question.body).to eq "MyText"
+      end
+
+      it 're-render edit view' do
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:question) { create(:question) }
+
+    it "deletes a question" do
+      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+    end
+
+    it "redirects to #index" do
+      delete :destroy, params: { id: question }
+      expect(response).to redirect_to questions_path
     end
   end
 end
