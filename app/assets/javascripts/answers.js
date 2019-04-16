7d036d9a1d61f751830207f8cee891b54812d20a
CepCap <nikinoob@gmail.com>
2019-04-16 13:52:08 -0700

    views refactoring, tests refactoring, mainly for js reasons

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -2,7 +2,8 @@ $(document).on('turbolinks:load', function() {
   $('.edit-answer-link').on('click', function(e) {
     e.preventDefault();
     $(this).addClass('hidden');
-    var answerId = $(this).data('answerId');
-    $('div#edit-answer-' + answerId).removeClass('hidden');
+    var answerId = $(this).closest('.answer').data('answerId');
+    console.log(answerId)
+    $('[data-answer-id=' + answerId + '] .edit-answer').removeClass('hidden');
   })
 });
