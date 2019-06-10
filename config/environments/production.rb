9e9036998d57acb192e8eee7bdc24a99644c08d3
CepCap <nikinoob@gmail.com>
2019-06-10 11:14:56 -0700

    corrections

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,8 @@ Rails.application.configure do
     :port               => 587,
     :domain             => 'gmail.com',
     :authentication     => :plain,
-    :user_name          => Rails.application.credentials[Rails.env.to_sym][:mailer][:host],
-    :password           => Rails.application.credentials[Rails.env.to_sym][:mailer][:password]
+    :user_name          => Rails.application.credentials[:mailer][:host],
+    :password           => Rails.application.credentials[:mailer][:password]
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
